c455c7b7eea71b50d7112f73f4c8699fa216db94
varshashingote <120667268+varshashingote@users.noreply.github.com>
2022-12-25 11:54:40 +0530

    Add files via upload

---
+++ primefact.sh
@@ -0,0 +1,39 @@
+read -p "Enter the Number: " n
+if [ $n -lt 1 ];
+then
+echo "Not Accept"
+exit 1
+fi
+# find factors and prime
+
+i=2
+count=0
+flag=0
+for ((i;i<$n;));
+do
+    
+  if [ `expr $n % $i` -eq 0 ];
+then
+      factor=$i
+
+  for ((j=2;j<=`expr $factor / 2`;));
+      do
+      flag=0
+      if [ `expr $factor % $j` -eq 0 ];then
+      flag=1
+      break
+      fi
+      j=`expr $j + 1`
+  done
+  if [ $flag -eq 0 ];
+  then
+  echo "[ $factor ]"
+  count=1
+  fi
+  fi
+  i=`expr $i + 1`
+  done
+  if [ $count -eq 0 ];
+  then
+  echo "no prime factors found except 1 and $n"
+  fi
